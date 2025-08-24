7728da45347fede7b90e1980640307d2bc9e252f
evanafsana192-rgb <evanafsana192@gmail.com>
2025-08-24 16:38:25 +0600

    Update Ullash.js

--- Ullash.js
+++ Ullash.js
@@ -74,4 +74,39 @@ axios.get("https://raw.githubusercontent.com/cyber-ullash/cyber-bot/main/data.js
     });
 
 // Start the bot
-startBot();
+startBot();name: Node.js CI
+
+on:
+  push:
+    branches: [main]
+  pull_request:
+    branches: [main]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+        # See supported Node.js release schedule at https://nodejs.org/en/about/releases/
+
+    steps:
+    # Step to check out the repository code
+    - uses: actions/checkout@v2
+
+    # Step to set up the specified Node.js version
+    - name: Use Node.js ${{ matrix.node-version }}
+      uses: actions/setup-node@v2
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    # Step to install dependencies
+    - name: Install dependencies
+      run: npm install
+
+    # Step to run the bot with the correct port
+    - name: Start the bot
+      env:
+        PORT: 8080
+      run: npm start
